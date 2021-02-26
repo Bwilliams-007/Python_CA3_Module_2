3362be9ba281dcfcd7289d036af74f730c21f65d
Bwilliams-007 <babajidewilliamss@gmail.com>
2021-02-26 10:12:59 +0000

    First Commit

---
+++ CreateGetRequest_Moodle.py
@@ -0,0 +1,8 @@
+from requests import get, post
+import json
+from bs4 import BeautifulSoup
+
+KEY = "8cc87cf406775101c2df87b07b3a170d"
+URL = "https://034f8a1dcb5c.eu.ngrok.io"
+ENDPOINT = "/webservice/rest/server.php"
+
